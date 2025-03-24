bca79a511701b63425638436bd0f4fbac48cc8a7
willymariino <wg.mariino@gmail.com>
2025-03-24 17:04:46 +0100

    function people.js

--- people.js
+++ people.js
@@ -1,2 +1,10 @@
 const hobbies2 = require("./hobbies")
 const hobbies2 = require("./names")
+
+function peoples() {
+    hobbie("videogiochi", "lettura", "coding")
+    person("Anna", "Rossi")
+    return {
+
+    }
+}
