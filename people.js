fc9c86ec49643652dc2e86109c50868154fe251e
willymariino <wg.mariino@gmail.com>
2025-03-24 17:11:49 +0100

    object peoples

--- people.js
+++ people.js
@@ -2,8 +2,6 @@ const hobbies2 = require("./hobbies")
 const names2 = require("./names")
 
 function peoples() {
-    hobbies2("hobbie1", "hobbie2", "hobbie3")
-    names2("firstName", "lastName")
     return {
         hobbies2,
         names2
