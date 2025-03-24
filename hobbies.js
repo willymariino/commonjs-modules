a5cf68b7e72ec32007e7281ae373bbea052fcb90
willymariino <wg.mariino@gmail.com>
2025-03-24 16:54:34 +0100

    added hobbies.js

---
+++ hobbies.js
@@ -0,0 +1,8 @@
+function createHobbies(hobbie1, hobbie2, hobbie3) {
+    return [hobbie1, hobbie2, hobbie3]
+}
+
+const hobbie = createHobbies("videogiochi", "lettura", "coding")
+console.log(hobbie)
+
+module.export = createHobbies
--- names.js
+++ names.js
@@ -1,11 +1,11 @@
-function createPerson(firstName, lastName) {
+function createPerson(firstName, lastName) { // firstName e lastName = parametri segnaposto
     return {
         firstName,
         lastName
     }
 }
 
-const person = createPerson("Anna", "Rossi")
+const person = createPerson("Anna", "Rossi") //argomenti quando vado a chiamare la funzione
 console.log(person)
 
 module.export = createPerson
