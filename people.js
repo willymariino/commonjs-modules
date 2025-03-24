0833b147ad6aadfaf97dd89cf67d14f1d945f2b1
willymariino <wg.mariino@gmail.com>
2025-03-24 17:17:08 +0100

    changed names

--- people.js
+++ people.js
@@ -1,10 +1,10 @@
-const hobbies2 = require("./hobbies")
-const names2 = require("./names")
+const hobbies = require("./hobbies")
+const fullNames = require("./names")
 
 function peoples() {
     return {
-        hobbies2,
-        names2
+        hobbies,
+        fullNames
 
     }
 }
